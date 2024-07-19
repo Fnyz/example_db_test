93ea234854d60d9ea1fb6d9706d3a3a10f2e8602
fnyz <jezabellebusico@gmail.com>
2024-07-19 11:26:46 +0800

    hello

--- api/index.php
+++ api/index.php
@@ -1,22 +1,3 @@
 <?php
 
-// Load Laravel's autoload file
-require __DIR__ . '/../vendor/autoload.php';
-
-// Bootstrap Laravel application
-$app = require_once __DIR__ . '/../bootstrap/app.php';
-
-// Create an instance of the HTTP kernel
-$kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
-
-// Create a request object
-$request = Illuminate\Http\Request::capture();
-
-// Handle the request through the kernel
-$response = $kernel->handle($request);
-
-// Send the response back to the client
-$response->send();
-
-// Terminate the application
-$kernel->terminate($request, $response);
+require __DIR__ . '/../public/index.php';
--- routes/api.php
+++ routes/api.php
@@ -1,13 +1,29 @@
 <?php
-
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\DB;
+
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
 
-Route::get("/test", function(){
-    return response()->json(["message" => "Hello World"]);
+Route::get("/test", function() {
+    $users = User::all();
+    return response()->json([
+        "message" => "Hello World",
+        "users" => $users
+    ]);
+});
+
+
+Route::get('/test-db-connection', function () {
+    try {
+        DB::connection()->getPdo();
+        return 'Database connection is successful!';
+    } catch (\Exception $e) {
+        return 'Failed to connect to the database: ' . $e->getMessage();
+    }
 });
